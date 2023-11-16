ac3024ed758bbd6b2a9fe595a93df8b362136f58
Antara999333 <143966099+Antara999333@users.noreply.github.com>
2023-11-15 21:21:31 -0500

    Update Analyze songs data.sql

--- Analyze songs data.sql
+++ Analyze songs data.sql
@@ -29,3 +29,19 @@ WHERE
   AND
   tempo BETWEEN 100 AND 140;
 
+-- Which artists released the most songs each year?
+SELECT
+    artist_name,
+    COUNT(artist_name) AS num_songs,
+    year
+FROM
+    prepared_song_data
+WHERE
+    year > 0
+GROUP BY
+    artist_name,
+    year
+ORDER BY
+    num_songs DESC,
+    year DESC;
+
